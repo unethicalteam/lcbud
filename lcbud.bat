b4cbd54ee523a2f573081aca444241537f92475c
Jason <38664452+uchks@users.noreply.github.com>
2023-09-20 23:25:46 -0500

    update: 2.4
    remove:
    - blurer (temporarily)
    - KeyboardFix
    - Fractal
    
    update:
    - legit-ish
    - lunar client download link
    - weave loader
    
    fix:
    - prevent github from converting batch scripts to unix line endings (LF)

--- lcbud.bat
+++ lcbud.bat
@@ -1,8 +1,8 @@
 @echo off
 :: Sets Defaults
 setlocal EnableDelayedExpansion
-set ver=v2.3
-set lunarver=3.0.9
+set ver=v2.4
+set lunarver=3.1.0
 set "modtargetDirectory=%userprofile%\.weave\mods"
 title lcbud %ver%
 
@@ -85,7 +85,7 @@ set /p "input=Enter the corresponding number and press Enter: "
 set "applicationData[1]=https://github.com/Youded-byte/lunar-client-qt/releases/latest/download/windows.zip lcqt.zip"
 set "applicationData[2]=https://github.com/uchks/notqt2/releases/latest/download/windows-portable.zip lcqt2-nils.zip"
 set "applicationData[3]=https://github.com/Nilsen84/lunar-launcher-inject/releases/download/v1.3.0/lunar-launcher-inject-windows-1.3.0.exe lunar-launcher-inject.exe"
-set "applicationData[4]=https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.3/Weave-Loader-Agent-0.2.3.jar Weave-Loader-Agent-0.2.3.jar"
+set "applicationData[4]=https://github.com/Weave-MC/Weave-Loader/releases/download/v0.2.4/Weave-Loader-Agent-0.2.4.jar Weave-Loader-Agent-0.2.4.jar"
 
 :: Iterate through options and handle downloads
 for %%i in (1 2 3 4) do (
@@ -237,68 +237,64 @@ echo.
 echo   Mods:
 echo   1) Alex Fix - Fixes a bug where Alex's arms are shifted down lower than Steve's.
 echo   2) Blood Kill Effect - Redstone particles when a player dies.
-echo   3) Blurer - A mod that gives you smoooooth blur effects. (ZenitchCore Required, Temporarily Not Available)
-echo   4) Cracked Account - Sail the high seas with a poor person's account.
-echo   5) Crepes - Display MinecraftCapes users' capes in-game. (ZenithCore Required)
-echo   6) Dulikk - Custom View Model
-echo   7) FPS - Set a custom FPS limit.
-echo   8) Good Game - AutoGG for more servers.
-echo   9) GTB Solver - Guess the builder solver.
-echo  10) Hit Fire - Hitting an entity sets it on fire for 1.5 seconds.
-echo  11) Hu Tao - Draw a dancing Hu Tao on your screen.
-echo  12) Inclumsy - Spoof your ping.
-echo  13) KeyboardFix - Fix for Linux users where "Shift-2" and "Shift-6" key combinations don't work.
-echo  14) MMUtils - Hypixel Murder Mystery Utilities.
-echo  15) Name History - Check a user's name history with a command.
-echo  16) OofMod - Play a customizable sound during in-game events.
-echo  17) PitUtils - Hypixel Pit Utilities.
-echo  18) Projectile Trail - Creates particles behind arrows, fishing rod hooks, and throwables.
-echo  19) Raw Input (Not Needed for LCQT2)
-echo  20) Sulfur - A port of Phosphor.
-echo  21) Toggle Bobbing - Quickly toggle view bobbing. (ZenithCore Required)
-echo  22) Toggle Chat - Quickly toggle chat. (ZenithCore Required)
-echo  23) VanillaMenu - Bring back the vanilla Minecraft main menu.
-echo  24) ViaLunar - Join 1.9+ servers from LunarClient 1.8 (ZenithCore Required)
-echo  25) WeaveChamsMod - Render players over blocks, see players through walls!
-echo  26) Weave Optifine URL - Specify a new URL for the Optifine cape server.
-echo  27) WeaveQuickReport - quickreport mod rewritten for Weave. Also comes with autododge.
-echo  28) ZenithCore - Dependency for mods.
+echo   3) Cracked Account - Sail the high seas with a poor person's account.
+echo   4) Crepes - Display MinecraftCapes users' capes in-game. (ZenithCore Required)
+echo   5) Dulikk - Custom View Model
+echo   6) FPS - Set a custom FPS limit.
+echo   7) Good Game - AutoGG for more servers.
+echo   8) GTB Solver - Guess the builder solver.
+echo   9) Hit Fire - Hitting an entity sets it on fire for 1.5 seconds.
+echo  10) Hu Tao - Draw a dancing Hu Tao on your screen.
+echo  11) Inclumsy - Spoof your ping.
+echo  12) MMUtils - Hypixel Murder Mystery Utilities.
+echo  13) Name History - Check a user's name history with a command.
+echo  14) OofMod - Play a customizable sound during in-game events.
+echo  15) PitUtils - Hypixel Pit Utilities.
+echo  16) Projectile Trail - Creates particles behind arrows, fishing rod hooks, and throwables.
+echo  17) Raw Input (Not Needed for LCQT2)
+echo  18) Sulfur - A port of Phosphor.
+echo  19) Toggle Bobbing - Quickly toggle view bobbing. (ZenithCore Required)
+echo  20) Toggle Chat - Quickly toggle chat. (ZenithCore Required)
+echo  21) VanillaMenu - Bring back the vanilla Minecraft main menu.
+echo  22) ViaLunar - Join 1.9+ servers from LunarClient 1.8 (ZenithCore Required)
+echo  23) WeaveChamsMod - Render players over blocks, see players through walls!
+echo  24) Weave Optifine URL - Specify a new URL for the Optifine cape server.
+echo  25) WeaveQuickReport - quickreport mod rewritten for Weave. Also comes with autododge.
+echo  26) ZenithCore - Dependency for mods.
 echo   -------------------------------------------
-echo  29) Cheats
-echo  30) Go Back
+echo  27) Cheats
+echo  28) Go Back
 echo.
 set /p mods="Select an option: "
 
 set "modData[1]=https://github.com/Syz66/AlexFix/releases/download/1.1/AlexFix-1.1.jar AlexFix.jar"
 set "modData[2]=https://github.com/Syz66/BloodKillEffect/releases/download/1.1/BloodKillEffect-1.1.jar BloodKillEffect.jar"
-set "modData[3]=https://gitlab.com/candicey-weave/blurer/uploads/618313a501dadedc513b190a432d64ed/Blurer-0.1.0.jar Blurer.jar"
-set "modData[4]=https://gitlab.com/candicey-weave/cracked-account/uploads/c351fccaba7d5aabc6746bff63137ba6/WeaveCrackedAccount-0.2.1.jar WeaveCrackedAccount.jar"
-set "modData[5]=https://gitlab.com/candicey-weave/crepes/uploads/94b6d61337cc03c8e55745cce5845bce/Crepes-0.1.1.jar Crepes.jar"
-set "modData[6]=https://gitlab.com/candicey-weave/dulikk/uploads/2f0c733b0ab6fa1ccd644c80b0c07eb1/Dulikk-0.1.0.jar Dulikk.jar"
-set "modData[7]=https://github.com/Syz66/FPS/releases/download/2.0/FPS-2.0.jar FPS.jar"
-set "modData[8]=https://github.com/Syz66/GG/releases/download/3.0/GG-3.0.jar GG.jar"
-set "modData[9]=https://gitlab.com/candicey-weave/gtb-solver/uploads/d1288dbce390dbe7d98cfc1f406750fd/GTB-Solver-0.1.0.jar GTB-Solver.jar"
-set "modData[10]=https://codeberg.org/chloe/HitFire/releases/download/1.0/HitFire-1.0.jar HitFire.jar"
-set "modData[11]=https://github.com/Ultramicroscope/HuTao/releases/download/v1.1/HuTao-1.0.jar HuTao.jar"
-set "modData[12]=https://codeberg.org/Candicey-Weave/Inclumsy/releases/download/v0.1.0/Inclumsy-0.1.0.jar Inclumsy.jar"
-set "modData[13]=https://github.com/koxx12-dev/Weave-KeyboardFix/releases/download/1.0/Weave-KeyboardFix-1.0.jar Weave-KeyboardFix.jar"
-set "modData[14]=https://github.com/Yan-Jobs/mm-utils/releases/download/v1/MMUtils-1.0.jar MMUtils.jar"
-set "modData[15]=https://github.com/Ultramicroscope/NameHistory/releases/download/weave-1.0/NameHistory-1.0.jar NameHistory.jar"
-set "modData[16]=https://github.com/thaYt/oofmod/releases/download/v1.0/OofMod-1.0.jar OofMod.jar"
-set "modData[17]=https://github.com/supercoolspy/PitUtilsLunar/releases/download/1.1.0/PitUtils-1.1.0.jar PitUtils.jar"
-set "modData[18]=https://codeberg.org/chloe/ProjectileTrail/releases/download/1.0/ProjectileTrail-1.0.jar ProjectileTrail.jar"
-set "modData[19]=https://github.com/koxx12-dev/Weave-Raw-Input/releases/download/1.0.1/RawInput-1.0.1.jar RawInput.jar"
-set "modData[20]=https://codeberg.org/chloe/Sulfur/releases/download/1.0/Sulfur-1.0.jar Sulfur.jar"
-set "modData[21]=https://github.com/Syz66/Toggle-Bobbing/releases/download/1.0/ToggleBobbing-1.0.jar ToggleBobbing.jar"
-set "modData[22]=https://github.com/Syz66/ToggleChat/releases/download/1.2/ToggleChat-1.2.jar ToggleChat.jar"
-set "modData[23]=https://github.com/Zxnii/VanillaMenu/releases/download/v3.0.0/VanillaMenu-3.0.0.jar VanillaMenu.jar"
-set "modData[24]=https://gitlab.com/candicey-weave/viaversion-lunar/uploads/9ba371b7abc07e0958ec559a9d9f0a30/ViaLunar-3.0.0.jar ViaLunar.jar"
-set "modData[25]=https://github.com/Tryflle/WeaveChamsMod/releases/download/1.0-beta/WeaveChamsMod-1.0.jar WeaveChamsMod.jar"
-set "modData[26]=https://github.com/770grappenmaker/weave-optifine-url/releases/download/v0.1/weave-optifine-url-0.1.jar weave-optifine-url.jar"
-set "modData[27]=https://github.com/Tryflle/WeaveQuickReport/releases/download/1.1/WeaveQuickReport-1.1.jar WeaveQuickReport.jar"
-set "modData[28]=https://codeberg.org/Candicey-Weave/Zenith-Core/releases/download/v1.3.7/Zenith-Core-1.3.7.jar Zenith-Core.jar"
+set "modData[3]=https://gitlab.com/candicey-weave/cracked-account/uploads/c351fccaba7d5aabc6746bff63137ba6/WeaveCrackedAccount-0.2.1.jar WeaveCrackedAccount.jar"
+set "modData[4]=https://gitlab.com/candicey-weave/crepes/uploads/94b6d61337cc03c8e55745cce5845bce/Crepes-0.1.1.jar Crepes.jar"
+set "modData[5]=https://gitlab.com/candicey-weave/dulikk/uploads/2f0c733b0ab6fa1ccd644c80b0c07eb1/Dulikk-0.1.0.jar Dulikk.jar"
+set "modData[6]=https://github.com/Syz66/FPS/releases/download/2.0/FPS-2.0.jar FPS.jar"
+set "modData[7]=https://github.com/Syz66/GG/releases/download/3.0/GG-3.0.jar GG.jar"
+set "modData[8]=https://gitlab.com/candicey-weave/gtb-solver/uploads/d1288dbce390dbe7d98cfc1f406750fd/GTB-Solver-0.1.0.jar GTB-Solver.jar"
+set "modData[9]=https://codeberg.org/chloe/HitFire/releases/download/1.0/HitFire-1.0.jar HitFire.jar"
+set "modData[10]=https://github.com/Ultramicroscope/HuTao/releases/download/v1.1/HuTao-1.0.jar HuTao.jar"
+set "modData[11]=https://codeberg.org/Candicey-Weave/Inclumsy/releases/download/v0.1.0/Inclumsy-0.1.0.jar Inclumsy.jar"
+set "modData[12]=https://github.com/Yan-Jobs/mm-utils/releases/download/v1/MMUtils-1.0.jar MMUtils.jar"
+set "modData[13]=https://github.com/Ultramicroscope/NameHistory/releases/download/weave-1.0/NameHistory-1.0.jar NameHistory.jar"
+set "modData[14]=https://github.com/thaYt/oofmod/releases/download/v1.0/OofMod-1.0.jar OofMod.jar"
+set "modData[15]=https://github.com/supercoolspy/PitUtilsLunar/releases/download/1.1.0/PitUtils-1.1.0.jar PitUtils.jar"
+set "modData[16]=https://codeberg.org/chloe/ProjectileTrail/releases/download/1.0/ProjectileTrail-1.0.jar ProjectileTrail.jar"
+set "modData[17]=https://github.com/koxx12-dev/Weave-Raw-Input/releases/download/1.0.1/RawInput-1.0.1.jar RawInput.jar"
+set "modData[18]=https://codeberg.org/chloe/Sulfur/releases/download/1.0/Sulfur-1.0.jar Sulfur.jar"
+set "modData[19]=https://github.com/Syz66/Toggle-Bobbing/releases/download/1.0/ToggleBobbing-1.0.jar ToggleBobbing.jar"
+set "modData[20]=https://github.com/Syz66/ToggleChat/releases/download/1.2/ToggleChat-1.2.jar ToggleChat.jar"
+set "modData[21]=https://github.com/Zxnii/VanillaMenu/releases/download/v3.0.0/VanillaMenu-3.0.0.jar VanillaMenu.jar"
+set "modData[22]=https://gitlab.com/candicey-weave/viaversion-lunar/uploads/9ba371b7abc07e0958ec559a9d9f0a30/ViaLunar-3.0.0.jar ViaLunar.jar"
+set "modData[23]=https://github.com/Tryflle/WeaveChamsMod/releases/download/1.1-Release/WeaveChamsMod-1.1.jar WeaveChamsMod.jar"
+set "modData[24]=https://github.com/770grappenmaker/weave-optifine-url/releases/download/v0.1/weave-optifine-url-0.1.jar weave-optifine-url.jar"
+set "modData[25]=https://github.com/Tryflle/WeaveQuickReport/releases/download/1.1/WeaveQuickReport-1.1.jar WeaveQuickReport.jar"
+set "modData[26]=https://codeberg.org/Candicey-Weave/Zenith-Core/releases/download/v1.3.7/Zenith-Core-1.3.7.jar Zenith-Core.jar"
 
-for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28) do (
+for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26) do (
     for /f "tokens=1,2" %%a in ("!modData[%%i]!") do (
         if "%mods%"=="%%i" (
             call :DownloadFile "%%a" "%%b"
@@ -308,8 +304,8 @@ for %%i in (1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
     )
 )
 
-if /i "%mods%"=="30" goto :modscheats
-if /i "%mods%"=="31" goto :am
+if /i "%mods%"=="27" goto :modscheats
+if /i "%mods%"=="28" goto :am
 goto :mods
 
 :modscheats
@@ -322,25 +318,23 @@ echo   [36mhttps://discord.gg/vhJ8Dsp9qa[0m
 echo.
 echo   Cheats:
 echo   1) Blue Client [40;31m(Cheating Client)[40;37m
-echo   2) Fractal [40;31m(Cheating Client)[40;37m
-echo   3) Legit-ish [40;31m(Cheating Client)[40;37m
-echo   4) NoHitDelay (Not needed for LCQT2)
-echo   5) RavenWeave [40;31m(Cheating Client[40;37m, ZenithCore Required)
-echo   6) RavenWeaveLite [40;31m(Cheating Client)[40;37m
-echo   7) VapeFix (Requires Vape V4/Lite)
-echo   8) Go Back
+echo   2) Legit-ish [40;31m(Cheating Client)[40;37m
+echo   3) NoHitDelay (Not needed for LCQT2)
+echo   4) RavenWeave [40;31m(Cheating Client[40;37m, ZenithCore Required)
+echo   5) RavenWeaveLite [40;31m(Cheating Client)[40;37m
+echo   6) VapeFix (Requires Vape V4/Lite)
+echo   7) Go Back
 echo.
 set /p modscheats="Select an option: "
 
 set "cheatData[1]=https://github.com/kacorvixon1337/blueclient/releases/download/release/blue-client.jar BlueClient"
-set "cheatData[2]=https://github.com/AriaJackie/Fractal/releases/download/release-1.2/fractal-weave-1.2.jar Fractal"
-set "cheatData[3]=https://github.com/legitish/Legit-ish-Weave/releases/download/v2.0.0-beta/legitish-2.0.0-beta.jar Legit-ish"
-set "cheatData[4]=https://github.com/Nilsen84/WeaveNoHitDelay/releases/download/v2.0/WeaveNoHitDelay-2.0.jar NoHitDelay"
-set "cheatData[5]=https://github.com/PianoPenguin471/RavenWeave/releases/download/1.1.3/RavenWeave-1.1.3.jar RavenWeave"
-set "cheatData[6]=https://github.com/PianoPenguin471/RavenWeaveLite/releases/download/bugfix/RavenWeaveLite-1.0.jar RavenWeaveLite"
-set "cheatData[7]=https://github.com/Syz66/VapeFix/releases/download/1.1/VapeFix-1.1.jar VapeFix"
+set "cheatData[2]=https://github.com/legitish/Legit-ish-Weave/releases/download/latest/legitish-2.0.0.jar Legit-ish"
+set "cheatData[3]=https://github.com/Nilsen84/WeaveNoHitDelay/releases/download/v2.0/WeaveNoHitDelay-2.0.jar NoHitDelay"
+set "cheatData[4]=https://github.com/PianoPenguin471/RavenWeave/releases/download/1.1.3/RavenWeave-1.1.3.jar RavenWeave"
+set "cheatData[5]=https://github.com/PianoPenguin471/RavenWeaveLite/releases/download/bugfix/RavenWeaveLite-1.0.jar RavenWeaveLite"
+set "cheatData[6]=https://github.com/Syz66/VapeFix/releases/download/1.1/VapeFix-1.1.jar VapeFix"
 
-for %%i in (1 2 3 4 5 6 7) do (
+for %%i in (1 2 3 4 5 6) do (
     for /f "tokens=1,2" %%a in ("!cheatData[%%i]!") do (
         if "%modscheats%"=="%%i" (
             call :DownloadFile "%%a" "%%b.jar"
@@ -467,7 +461,7 @@ echo   [36mhttps://discord.gg/vhJ8Dsp9qa[0m
 echo.
 echo   Lunar Client QTs:
 echo     Youded-Byte (QT) - [36mhttps://github.com/Youded-byte/lunar-client-qt[0m
-echo     Nilsen84 (QT 2.0) - [36mhttps://github.com/Nilsen84/lcqt2[0m
+echo     Nilsen84 (QT 2.0) - [36mhttps://github.com/Nilsen84[0m
 echo.
 echo   Lunar Client Launcher Inject:
 echo     Nilsen84 - [36mhttps://github.com/Nilsen84/lunar-launcher-inject[0m
@@ -476,7 +470,7 @@ echo   Weave Loader:
 echo     Weave Maintainers - [36mhttps://github.com/Weave-MC[0m
 echo.
 echo   Agents:
-echo     Nilsen84 - [36mhttps://github.com/Nilsen84/[0m
+echo     Nilsen84 - [36mhttps://github.com/Nilsen84[0m
 echo.
 echo   Press any key to go back...
 pause >nul
